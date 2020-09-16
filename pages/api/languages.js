890e8750dbed544d5d9a95569a7375911dc47af5
abeidahmed <abeidahmed92@gmail.com>
2020-09-16 21:40:41 +0530

    add demo display code

--- pages/api/languages.js
+++ pages/api/languages.js
@@ -3,11 +3,35 @@ const languages = [
     id: 1,
     name: 'Ruby',
     dasherizedName: 'ruby',
+    demo: `
+      def template_list
+        component = Template::ComponentFinder.new
+        page = Template::PageFinder.new
+        [component, page]
+      end
+
+      def template_type
+        template_list.find { |type| type.match?(@type) }
+      end
+    `,
   },
   {
     id: 2,
     name: 'Javascript',
     dasherizedName: 'js',
+    demo: `
+      const pluckDeep = key => obj => key.split('.').reduce((accum, key) => accum[key], obj)
+
+      const compose = (...fns) => res => fns.reduce((accum, next) => next(accum), res)
+
+      const unfold = (f, seed) => {
+        const go = (f, seed, acc) => {
+          const res = f(seed)
+          return res ? go(f, res[1], acc.concat([res[0]])) : acc
+        }
+        return go(f, seed, [])
+      }
+    `,
   },
 ];
 
