b79af2a38b7a9d0768ff65a1f2b7c1d7a06c7882
abeidahmed <abeidahmed92@gmail.com>
2020-09-16 21:22:27 +0530

    install prismjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/font.css';
 import '../styles/index.css';
+import '../vendor/js/prism-live';
+import '../vendor/css/prism-live.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
