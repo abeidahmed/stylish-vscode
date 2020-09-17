d02cb68e540c6ebef2e42b5f0622b0e2f7d19317
abeidahmed <abeidahmed92@gmail.com>
2020-09-17 17:31:45 +0530

    improve language filter on user search

--- pages/api/languages.js
+++ pages/api/languages.js
@@ -4,18 +4,28 @@ const languages = [
   {
     id: 1,
     name: 'Ruby',
+    searchTag: ['ruby', 'ruby-on-rails', 'rails', 'rb'],
   },
   {
     id: 2,
     name: 'JavaScript',
+    searchTag: ['js', 'javascript'],
   },
 ];
 
 export default (req, res) => {
   let search = req.query.search;
-  const filteredLang = languages.filter(
-    ({ name }) => name.toLowerCase().indexOf(dasherized(search)) !== -1
-  );
+  const filteredLang = filterable(search);
   const sorted = filteredLang.sort((a, b) => (a.name > b.name ? 1 : -1));
   res.status(200).json(sorted);
 };
+
+function filterable(search) {
+  return languages.filter((ele) => {
+    if (!search) return ele;
+    return (
+      ele.searchTag.indexOf(dasherized(search)) !== -1 ||
+      ele.searchTag.some((tag) => tag.startsWith(dasherized(search)))
+    );
+  });
+}
