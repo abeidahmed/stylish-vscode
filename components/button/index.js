4548639c532075cc3031a3a5a3756b941ee62959
abeidahmed <abeidahmed92@gmail.com>
2020-09-17 23:56:54 +0530

    update active button icon color

--- components/button/index.js
+++ components/button/index.js
@@ -11,7 +11,7 @@ export function Button({ active, title, children, ...props }) {
     >
       {children}
       {active === title && (
-        <Icon icon="code" className="w-5 h-5 text-gray-500" />
+        <Icon icon="code" className="w-5 h-5 text-teal-400" />
       )}
     </button>
   );
