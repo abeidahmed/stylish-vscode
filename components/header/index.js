7647e1b2f492181667b7e55b834d62f5fdb8f518
abeidahmed <abeidahmed92@gmail.com>
2020-09-17 18:21:45 +0530

    add twitter share link

--- components/header/index.js
+++ components/header/index.js
@@ -14,10 +14,12 @@ export function Header() {
           </i>
         </a>
         <a
-          href="#"
+          href="https://twitter.com/intent/tweet?text=Style%20your%20vscode%20the%20way%20you%20like&url=https%3A%2F%2Fsuper-rentals.example.com&hashtags=vscode%2Cvscodethemes%2Cstyles&via=iamhawaabi"
+          target="_blank"
+          rel="external nofollow noreferrer noopener"
           className="text-gray-100 transition duration-150 ease-in-out hover:text-teal-400"
         >
-          <span className="sr-only">Twitter profile</span>
+          <span className="sr-only">Share this website on twitter</span>
           <i>
             <Icon icon="twitter" className="w-6 h-6" standalone="true" />
           </i>
