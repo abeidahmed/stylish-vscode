eaae023be6856c320075372ca7f5d204259e7608
abeidahmed <abeidahmed92@gmail.com>
2020-09-17 19:14:26 +0530

    import all codemirror styles

--- pages/api/themes.js
+++ pages/api/themes.js
@@ -3,20 +3,157 @@ import { dasherized } from '../../utils/helpers';
 
 const themes = [
   {
-    id: 1,
-    name: 'dracula',
+    name: 'ambiance',
   },
   {
-    id: 2,
-    name: 'material',
-  },
-  {
-    id: 3,
     name: 'ayu-dark',
   },
   {
-    id: 4,
     name: 'ayu-mirage',
+  },
+  {
+    name: 'base16-dark',
+  },
+  {
+    name: 'base16-light',
+  },
+  {
+    name: 'bespin',
+  },
+  {
+    name: 'blackboard',
+  },
+  {
+    name: 'cobalt',
+  },
+  {
+    name: 'colorforth',
+  },
+  {
+    name: 'darcula',
+  },
+  {
+    name: 'dracula',
+  },
+  {
+    name: 'duotone-dark',
+  },
+  {
+    name: 'duotone-light',
+  },
+  {
+    name: 'eclipse',
+  },
+  {
+    name: 'elegant',
+  },
+  {
+    name: 'erlang-dark',
+  },
+  {
+    name: 'gruvbox-dark',
+  },
+  {
+    name: 'hopscotch',
+  },
+  {
+    name: 'material',
+  },
+  {
+    name: 'material-darker',
+  },
+  {
+    name: 'material-ocean',
+  },
+  {
+    name: 'material-palenight',
+  },
+  {
+    name: 'mbo',
+  },
+  {
+    name: 'mdn-like',
+  },
+  {
+    name: 'midnight',
+  },
+  {
+    name: 'monokai',
+  },
+  {
+    name: 'moxer',
+  },
+  {
+    name: 'neat',
+  },
+  {
+    name: 'nord',
+  },
+  {
+    name: 'oceanic-next',
+  },
+  {
+    name: 'panda-syntax',
+  },
+  {
+    name: 'paraiso-dark',
+  },
+  {
+    name: 'paraiso-light',
+  },
+  {
+    name: 'pastel-on-dark',
+  },
+  {
+    name: 'railscasts',
+  },
+  {
+    name: 'rubyblue',
+  },
+  {
+    name: 'seti',
+  },
+  {
+    name: 'shadowfox',
+  },
+  {
+    name: 'solarized',
+  },
+  {
+    name: 'ssms',
+  },
+  {
+    name: 'the-matrix',
+  },
+  {
+    name: 'tomorrow-night-bright',
+  },
+  {
+    name: 'tomorrow-night-eighties',
+  },
+  {
+    name: 'ttcn',
+  },
+  {
+    name: 'twilight',
+  },
+  {
+    name: 'vibrant-ink',
+  },
+  {
+    name: 'xq-dark',
+  },
+  {
+    name: 'xq-light',
+  },
+  {
+    name: 'yeti',
+  },
+  {
+    name: 'yonce',
+  },
+  {
+    name: 'zenburn',
   },
 ];
 
