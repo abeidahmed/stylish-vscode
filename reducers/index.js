13c9f03cae4dffa517203a12694fde0d3821154a
abeidahmed <abeidahmed92@gmail.com>
2020-09-18 21:47:46 +0530

    refactor reducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,29 +1,11 @@
 export function reducer(state, action) {
   switch (action.type) {
-    case 'UPDATE_FONT_SIZE':
+    case 'UPDATE_FIELD':
       return {
         ...state,
-        fontSize: action.fontSize,
+        [action.field]: action.value,
       };
-    case 'UPDATE_LINE_HEIGHT':
-      return {
-        ...state,
-        lineHeight: action.lineHeight,
-      };
-    case 'TOGGLE_LINE_NUMBER':
-      return {
-        ...state,
-        showLineNumber: action.showLineNumber,
-      };
-    case 'TOGGLE_DARK_THEME':
-      return {
-        ...state,
-        showDarkTheme: action.showDarkTheme,
-      };
-    case 'TOGGLE_LIGHT_THEME':
-      return {
-        ...state,
-        showLightTheme: action.showLightTheme,
-      };
+    default:
+      return state;
   }
 }
