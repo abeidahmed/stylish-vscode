3399c1b2ecf0c0522b03ab3ce599a1cff7cdc0e9
abeidahmed <abeidahmed92@gmail.com>
2020-09-18 22:14:09 +0530

    add padding on main element

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,10 @@ export default function Home() {
       <div className="min-h-screen font-sans antialiased text-gray-300 bg-gray-900">
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <Header />
-          <main style={{ minHeight: 'calc(100vh - 64px)' }}>
+          <main
+            className="pt-4 pb-10"
+            style={{ minHeight: 'calc(100vh - 64px)' }}
+          >
             <div>
               <Hero />
               <div className="flex flex-col w-full max-w-3xl p-6 mx-auto mt-12 bg-gray-800 rounded-md shadow-lg">
