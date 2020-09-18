ea5c059827fb329e6b6c50f3197acb6f17e4b3e0
abeidahmed <abeidahmed92@gmail.com>
2020-09-18 12:42:52 +0530

    update twitter share url

--- components/header/index.js
+++ components/header/index.js
@@ -16,7 +16,7 @@ export function Header() {
           </i>
         </a>
         <a
-          href="https://twitter.com/intent/tweet?text=Style%20your%20vscode%20the%20way%20you%20like&url=https%3A%2F%2Fstylish-vscode.vercel.app&hashtags=vscode%2Cvscodethemes%2Cstyles&via=iamhawaabi"
+          href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fstylish-vscode.vercel.app%2F&text=Style%20your%20vscode%20like%20the%20way%20you%20want.%20Pick%20themes%20and%20fonts%20for%20your%20coding%20language%2C%20and%20get%20started.%20By%20%40iamhawaabi%20%23vscode"
           target="_blank"
           rel="external nofollow noreferrer noopener"
           className="text-gray-100 transition duration-150 ease-in-out hover:text-teal-400"
