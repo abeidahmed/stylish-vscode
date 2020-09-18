5a4828ee49a86d96c4111b8f1fd2270103013bd8
abeidahmed <abeidahmed92@gmail.com>
2020-09-18 19:50:34 +0530

    truncate long text on button and input value

--- components/button/index.js
+++ components/button/index.js
@@ -9,9 +9,9 @@ export function Button({ active, title, children, ...props }) {
       } flex items-center justify-between w-full text-left px-3 py-2.5 sm:py-2 text-gray-200 leading-5 focus:outline-none focus:bg-gray-800 hover:bg-gray-800 border-t border-gray-800 first:border-t-0 transition duration-150 ease-in-out`}
       {...props}
     >
-      {children}
+      <span className="pr-2 truncate">{children}</span>
       {active === title && (
-        <Icon icon="code" className="w-5 h-5 text-teal-400" />
+        <Icon icon="code" className="flex-shrink-0 w-5 h-5 text-teal-400" />
       )}
     </button>
   );
--- components/fields/input.js
+++ components/fields/input.js
@@ -2,10 +2,13 @@ import { Icon } from '../icon';
 
 export function Field({ icon, label, ...props }) {
   return (
-    <div className="relative flex items-center transition duration-150 ease-in-out focus-within:text-teal-500">
-      <label className="absolute left-0 pl-10 text-gray-200 pointer-events-none">
+    <div className="relative flex items-center min-w-0 transition duration-150 ease-in-out focus-within:text-teal-500">
+      <span
+        className="absolute left-0 pl-10 text-gray-200 truncate pointer-events-none"
+        style={{ right: 40 }}
+      >
         {label}
-      </label>
+      </span>
       <input
         type="text"
         className="block w-full px-10 text-gray-200 transition duration-150 ease-in-out bg-transparent border-gray-400 shadow form-input focus:shadow-none focus:border-teal-400"
